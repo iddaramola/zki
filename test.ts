7b75c62055b89a5ef0be2c270ad287ec013d3566
Kizer <chuks.onuegbu@gmail.com>
2018-05-28 00:46:48 +0200

    first commit

---
+++ test.ts
@@ -0,0 +1,39 @@
+/**
+ * This function is meant to retrieve a list of all users from a server (assume request.get is an actual function)
+ * After retrieving the users, it retrieves the picture for each user (in a non-blocking manner) and patches the
+ * original user object with the newly retrieved photo URL.
+ */
+function getUsers(): Promise<User[]> {
+    return request.get<User[]>('/users').then(users => {
+        for (var i = 0; i < users.length; i++) {
+            const user = users[i];
+            getUserPhoto(user.id).then(photoData => user.photo = photoData.url);
+        }
+
+        return users;
+    });
+}
+
+/**
+ * This function retrieves a picture for a given user
+ * @param userId User to retrieve photo for
+ */
+function getUserPhoto(userId: string): Promise<UserPhoto> {
+    return request.get(`/users/${String(userId)}/photo`);
+}
+
+/**
+ * If you don't know TS and would rather use pure JS, you can delete this section and remove all type annotations above
+ */
+declare namespace request {
+    function get<T>(string): Promise<T>
+}
+
+declare type User = {
+    id: string;
+    photo?: string;
+}
+
+declare type UserPhoto = {
+    url: string;
+}
