c22e5e42815ae432f41a7305e8eec8e9d96c0eb8
Idowu Daramola <idowu.daramola@gmail.com>
2018-05-30 16:24:47 +0100

    changed both functions 'getUsers' and 'getUserPhoto' to use async/await, and re-wrote the loop to '.map' instead of 'for-loop'

--- test.ts
+++ test.ts
@@ -3,35 +3,37 @@
  * After retrieving the users, it retrieves the picture for each user (in a non-blocking manner) and patches the
  * original user object with the newly retrieved photo URL.
  */
-function getUsers(): Promise<User[]> {
+async function getUsers() {
+return await request.get('/users').then(users =>{ 
+    let newUsers = users.map((user)=> getUserPhoto(user.id).then(photoData => user.photo = photoData.url))
+    });
 
-    return request.get<User[]>('/users').then(users => {
-
+    /*
         for (var i = 0; i < users.length; i++) {
             //var user = users[i];
+
+            // MY CHANGES EXPLAINED BELOW
+            
             //in other to successfully patch the original user objects with photo data,
             //there is no need to declare a seperate variable 'user' for each iteration
             //of the for-loop, as this results in no effective change to the original user
             //objects. to fix the bug , I delected the variable declaration line and change
             //all occurences of 'user.id' to 'users[i].id'
+
+
             getUserPhoto(users[i].id).then(photoData => users[i].photo = photoData.url);
         }
 
         return users;
-    });
+        */
 }
-
-/**
- * This function retrieves a picture for a given user
- * @param userId User to retrieve photo for
- */
-function getUserPhoto(userId: string): Promise<UserPhoto> {
-    return request.get(`/users/${String(userId)}/photo`);
+async function getUserPhoto(userId) {
+      return await request.get(`/users/${String(userId)}/photo`);
 }
 
 /**
  * If you don't know TS and would rather use pure JS, you can delete this section and remove all type annotations above
- */
+ 
 declare namespace request {
     function get<T>(string): Promise<T>
 }
@@ -43,4 +45,4 @@ declare type User = {
 
 declare type UserPhoto = {
     url: string;
-}
+}*/
