56515495f4a5a9129146d3a9b5902b005f736618
Idowu Daramola <idowu.daramola@gmail.com>
2018-06-04 01:16:20 +0100

    changed 'getUsers function to return new patched users

--- test.ts
+++ test.ts
@@ -4,57 +4,20 @@
  * original user object with the newly retrieved photo URL.
  */
  export async function getUsers() {
-//return await request.get('/users').then(users =>{ 
-  //  let newUsers = users.map( user => getUserPhoto(user.id).then(photoData => user.photo = photoData.url))
-  //  });
-    
-    const users = await request.get('/users');
-     // this is a non-blocking loop
-     users.map(async (user) => {
-           const userPhoto =  await getUserPhoto(user.id)
-           user.photo = userPhoto.url;
-           return user;
-          }
-      )
 
-    
-    /*
-        for (var i = 0; i < users.length; i++) {
-            //var user = users[i];
+          const users = await request.get('/users');
+           // this is a non-blocking loop
+           let newUsers = users.map(async (user) => {
+                                 const userPhoto =  await getUserPhoto(user.id)
+                                 user.photo = userPhoto.url;
+                                 return user;
+                                }
+                        )
+            return newUsers;
 
-            // MY CHANGES EXPLAINED BELOW
-
-            //in other to successfully patch the original user objects with photo data,
-            //there is no need to declare a seperate variable 'user' for each iteration
-            //of the for-loop, as this results in no effective change to the original user
-            //objects. to fix the bug , I delected the variable declaration line and change
-            //all occurences of 'user.id' to 'users[i].id'
-
-
-            getUserPhoto(users[i].id).then(photoData => users[i].photo = photoData.url);
-        }
-
-        return users;
-        */
 }
  export async function getUserPhoto(userId) {
       return await request.get(`/users/${String(userId)}/photo`);
 }
 
 
-
-/**
- * If you don't know TS and would rather use pure JS, you can delete this section and remove all type annotations above
- 
-declare namespace request {
-    function get<T>(string): Promise<T>
-}
-
-declare type User = {
-    id: string;
-    photo?: string;
-}
-
-declare type UserPhoto = {
-    url: string;
-}*/
